862a090d90bf3b9b3af9228cf9212ea8067f6227
Aditya <mehrotra.aditya16@gmail.com>
2025-08-07 12:34:40 -0700

    first commit

---
+++ src/components/aditya.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>Hello, my name is Aditya!</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Aditya",
+};
+</script>
+
+<style scoped>
+h2 {
+  color: #42b983;
+}
+</style>
